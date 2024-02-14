0b4a6412b1f443b9e878939abd0cae7e7630f191
Nurmx7292 <raxmet2004@mail.ru>
2024-02-14 22:22:55 +0100

    Added tests for the Utilities Static class

---
+++ UtilitiesTests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Utilities;
+namespace UtilitiesTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test LongWordsCount
+            int longWordsCount1 = StringUtilities.LongWordsCount("Some words to count");
+            Console.WriteLine($"LongWordsCount : {longWordsCount1}");
+
+            int longWordsCount2 = StringUtilities.LongWordsCount("The lord of the rings");
+            Console.WriteLine($"LongWordsCount2: {longWordsCount2}");
+
+            // Test ToSpinalCase
+            string spinalCase1 = StringUtilities.ToSpinalCase("spinal Case Testing");
+            Console.WriteLine($"ToSpinalCase1: {spinalCase1}");
+
+            string spinalCase2 = StringUtilities.ToSpinalCase("the LorD OF thE Rings");
+            Console.WriteLine($"ToSpinalCase2: {spinalCase2}");
+
+            string pascalCase1 = "hElLo woRlD)".ToPascalCase();
+            Console.WriteLine($"ToPascalCase1: {pascalCase1}");
+
+            string pascalCase2 = "the LorD OF thE Rings".ToPascalCase();
+            Console.WriteLine($"ToPascalCase2: {pascalCase2}");
+        }
+    }
+}
